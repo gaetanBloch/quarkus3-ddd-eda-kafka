fbf6d0b1f24c890deb3c66b8a23bd129e40a80d8
gbloch <gaetan.bloch@gmail.com>
2023-04-15 07:59:26 +0200

    chore: install nvm, npm and pnpm

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# Install node
+nvm install 18.16.0
+
+# Install pnpm
+npm init -y && \
+npm i -g pnpm && \
+pnpm add -g pnpm && \
+pnpm config set store-dir ~/.pnpm-store && \
+rm -f node_modules && \
+rm -f package-lock.json && \
+pnpm i
